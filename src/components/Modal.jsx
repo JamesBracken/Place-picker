7bb77d7d672f2738a0cc6a560e0e4d6d7d22142c
James <james_bracken@yahoo.com>
2024-06-24 21:30:35 +0100

    .

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,26 +1,14 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { useRef } from 'react';
 import { createPortal } from 'react-dom';
 
-const Modal = forwardRef(function Modal({ children }, ref) {
-  const dialog = useRef();
-
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => {
-        dialog.current.showModal();
-      },
-      close: () => {
-        dialog.current.close();
-      },
-    };
-  });
+function Modal({ open, children }) {
+  const dialog = useRef()
 
   return createPortal(
-    <dialog className="modal" ref={dialog}>
-      {children}
+    <dialog className="modal" ref={dialog} open={open}>
+      {children} 
     </dialog>,
     document.getElementById('modal')
   );
-});
-
-export default Modal;
+}
+export default Modal
